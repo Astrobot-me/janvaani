923f60dc1ae3549fd70ae018800a9b40f8c4c75a
ashwani-sinha <ashwaniraj084@gmail.com>
2025-09-23 21:31:01 +0530

    footer1

--- janvaani-fe/src/App.jsx
+++ janvaani-fe/src/App.jsx
@@ -1,5 +1,5 @@
-
 import './App.css'
+import Footer from './components/footer.jsx'
 
 function App() {
  
@@ -12,6 +12,37 @@ function App() {
       <p className="read-the-docs">
         lets start building it
       </p>
+            <h1>Welcome to JanVaani </h1>
+      
+      <p className="read-the-docs">
+        lets start building it
+      </p>
+            <h1>Welcome to JanVaani </h1>
+      
+      <p className="read-the-docs">
+        lets start building it
+      </p>
+            <h1>Welcome to JanVaani </h1>
+      
+      <p className="read-the-docs">
+        lets start building it
+      </p>
+            <h1>Welcome to JanVaani </h1>
+      
+      <p className="read-the-docs">
+        lets start building it
+      </p>
+            <h1>Welcome to JanVaani </h1>
+      
+      <p className="read-the-docs">
+        lets start building it
+      </p>
+            <h1>Welcome to JanVaani </h1>
+      
+      <p className="read-the-docs">
+        lets start building it
+      </p>
+      <Footer/>
     </>
   )
 }
